fa4ec534fa3a9032aff41eab6e4eea390c0c5617
zspace09 <352045777@qq.com>
2020-05-21 20:39:04 +0800

    appium练习

--- p_appium/test_case/test_fist_appium.py
+++ p_appium/test_case/test_fist_appium.py
@@ -7,30 +7,45 @@
 # File    : test_fist_appium.py  
 #
 import json
+from time import sleep
 
 from appium import webdriver
 from selenium.webdriver.common.by import By
 
 
-def test_start():
-    desired_caps = {
-      "platformName": "Android",
-      "deviceName": "127.0.0.1:7555",
-      "appPackage": "com.xueqiu.android",
-      "appActivity": ".common.MainActivity",
-        "noRest": True
-    }
-    driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
-    driver.implicitly_wait(20)
-    els1 = driver.find_elements_by_id("com.xueqiu.android:id/title_text")
-    els2 = driver.find_elements_by_id("com.xueqiu.android:id/title_text")
-    el1 = driver.find_element_by_xpath(
-        "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.HorizontalScrollView/android.widget.FrameLayout/android.widget.LinearLayout[1]/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.FrameLayout[1]/android.widget.TextView")
-    el1.click()
-    el1.click()
-    el2 = driver.find_element_by_xpath(
-        "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.HorizontalScrollView/android.widget.FrameLayout/android.widget.LinearLayout[1]/android.widget.FrameLayout[3]/android.widget.LinearLayout/android.widget.FrameLayout[1]/android.widget.TextView")
-    el2.click()
+class TestAppium:
 
-    driver.quit()
+    def setup(self):
+        desired_caps = {
+            "platformName": "Android",
+            "deviceName": "127.0.0.1:7555",
+            "appPackage": "com.xueqiu.android",
+            "appActivity": ".view.WelcomeActivityAlias",
+            "noReset": True
+        }
+        self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+        self.driver.implicitly_wait(20)
 
+    def teardown(self):
+        self.driver.quit()
+
+    def test_start(self):
+        els1 = self.driver.find_elements_by_id("com.xueqiu.android:id/title_text")
+        els2 = self.driver.find_elements_by_id("com.xueqiu.android:id/title_text")
+        el1 = self.driver.find_element_by_xpath(
+            "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.HorizontalScrollView/android.widget.FrameLayout/android.widget.LinearLayout[1]/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.FrameLayout[1]/android.widget.TextView")
+        el1.click()
+        el2 = self.driver.find_element_by_xpath(
+            "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.HorizontalScrollView/android.widget.FrameLayout/android.widget.LinearLayout[1]/android.widget.FrameLayout[3]/android.widget.LinearLayout/android.widget.FrameLayout[1]/android.widget.TextView")
+        el2.click()
+
+    def test_search(self):
+        """
+        搜索框搜索阿里巴巴
+        判断是否有阿里巴巴
+        点击第一个阿里巴巴
+        :return:
+        """
+        self.driver.find_element_by_id('com.xueqiu.android:id/tv_search').click()
+        self.driver.find_element_by_id('com.xueqiu.android:id/search_input_text').send_keys("阿里巴巴")
+        sleep(5)
