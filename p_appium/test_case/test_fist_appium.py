ce7c79436b9b8c1a86fe62e3f30f3ea60dde5a1e
zspace09 <352045777@qq.com>
2020-05-22 16:25:11 +0800

    appium作业

--- p_appium/test_case/test_fist_appium.py
+++ p_appium/test_case/test_fist_appium.py
@@ -9,8 +9,11 @@
 import json
 from time import sleep
 
+import pytest
 from appium import webdriver
-from selenium.webdriver.common.by import By
+from appium.webdriver.common.mobileby import MobileBy
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class TestAppium:
@@ -24,28 +27,29 @@ class TestAppium:
             "noReset": True
         }
         self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
-        self.driver.implicitly_wait(20)
+        self.driver.implicitly_wait(10)
 
     def teardown(self):
         self.driver.quit()
 
-    def test_start(self):
-        els1 = self.driver.find_elements_by_id("com.xueqiu.android:id/title_text")
-        els2 = self.driver.find_elements_by_id("com.xueqiu.android:id/title_text")
-        el1 = self.driver.find_element_by_xpath(
-            "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.HorizontalScrollView/android.widget.FrameLayout/android.widget.LinearLayout[1]/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.FrameLayout[1]/android.widget.TextView")
-        el1.click()
-        el2 = self.driver.find_element_by_xpath(
-            "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.HorizontalScrollView/android.widget.FrameLayout/android.widget.LinearLayout[1]/android.widget.FrameLayout[3]/android.widget.LinearLayout/android.widget.FrameLayout[1]/android.widget.TextView")
-        el2.click()
-
-    def test_search(self):
+    @pytest.mark.parametrize("stock_name", ["阿里巴巴"])
+    def test_search(self, stock_name):
         """
         搜索框搜索阿里巴巴
         判断是否有阿里巴巴
         点击第一个阿里巴巴
+        点击添加自选按钮
+        断言，TextView内容变化
         :return:
         """
         self.driver.find_element_by_id('com.xueqiu.android:id/tv_search').click()
-        self.driver.find_element_by_id('com.xueqiu.android:id/search_input_text').send_keys("阿里巴巴")
-        sleep(5)
+        self.driver.find_element_by_id('com.xueqiu.android:id/search_input_text').send_keys(stock_name)
+        self.driver.find_element(MobileBy.XPATH, f"//*[@resource-id='com.xueqiu.android:id/name' and @text='{stock_name}']").click()
+        # 加自选
+        ele = self.driver.find_element(MobileBy.XPATH, f"//*[@resource-id='com.xueqiu.android:id/stockName' and @text='阿里巴巴']/../../android.widget.LinearLayout[3]/android.widget.TextView")
+        before = ele.text
+        ele.click()
+        WebDriverWait(self.driver, 10).until(expected_conditions.element_to_be_clickable((MobileBy.XPATH, f"//*[@resource-id='com.xueqiu.android:id/stockName' and @text='阿里巴巴']/../../android.widget.LinearLayout[3]/android.widget.TextView")))
+        after = ele.text
+        print(before, after)
+        assert before != after
